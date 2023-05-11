90ccfcb90e4bb0cb830224f2ae8d70db93cc9e01
Ved Patel <patelved44@outlook.com>
2023-05-11 19:25:15 -0400

    Add Ai

--- app/models/index.server.ts
+++ app/models/index.server.ts
@@ -1,7 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 type Message = {
-  text: string; 
+  text?: string; 
 };
 
 export async function getMessage(): Promise<Message> {
-  return { text: "Scars and a thrill seeker. Gods help her...." };
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "tell me a joke",
+    temperature: 1,
+    max_tokens: 128,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return { text: response.data.choices[0].text };
 }
