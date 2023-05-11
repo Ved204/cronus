5290e8f45cb2cef0d17891ccc95431906fb8b18a
Ved204 <92246817+Ved204@users.noreply.github.com>
2023-05-10 22:58:12 -0400

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,20 @@
-import { Link } from "remix";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "remix";
 import { useOptionalUser } from "~/utils";
+
+import { getMessage } from "~/models/index.server";
+
+export const loader = async () => {
+  return json({ message; await getMessage() });
+}
 
 export default function Index() {
   const user = useOptionalUser();
+  const { message } = useLoaderData<typeof loader>();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
-        Hello World!
+        {message.text}
       </div>
     </main>
   );
