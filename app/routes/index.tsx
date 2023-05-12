a901cccc18ff405b1f4a1a27c7a44a216f22faff
Ved Patel <patelved44@outlook.com>
2023-05-11 21:37:53 -0400

    test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,7 +20,7 @@ export default function Index() {
         {message.text}
       </div>
       <Form method="get">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={reload}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Tell me another
         </button>
       </Form>
