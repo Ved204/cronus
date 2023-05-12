2916af44f21dc39b8c752314419c0c994c568689
Ved Patel <patelved44@outlook.com>
2023-05-11 21:09:22 -0400

    Reload Button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,12 +11,12 @@ export default function Index() {
   const user = useOptionalUser();
   const { message } = useLoaderData<typeof loader>();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+    <main className="relative min-h-screen bg-white flex flex-col sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         {message.text}
       </div>
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={location.reload}>
           Tell me another
         </button>
       </div>
